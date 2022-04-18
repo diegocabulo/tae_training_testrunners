905b26ef21e1813016dc0d9ebc9fb01ad7870e63
Diego Gerardo Cabulo Perez <diego.cabulo@globant.com>
2022-04-18 15:45:28 -0500

    first commit

---
+++ src/test/java/com/globant/AppTest.java
@@ -0,0 +1,16 @@
+package com.globant;
+
+import org.testng.*;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void verify(){
+        System.out.println("holy");
+    }
+}
